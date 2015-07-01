8eea0a9d98bc64917e47056321258ed0c0f138aa
Qiming Fang <qf26@cornell.edu>
2015-07-01 12:29:02 -0700

    update getId module

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 module.exports = {
-  helloWorld: function() {
-    console.log("hello world");
+  generateId: function(userId, serviceName, serviceId) {
+    if (userId == undefined || userId == null ||
+      serviceName == undefined || serviceName == null ||
+      serviceId == undefined || serviceId == null) {
+      return null
+    }
+    return [userId, serviceName, serviceId].join("_");
   }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,23 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect
+  , service = require('../index');
 
 describe('Id Generation Module', function() {
-  it('should pass', function() {
-    expect(true).to.equal(true);
+  it('should return correct id', function() {
+    expect(service.generateId('a', 'b', 'c')).to.equal('a_b_c')
+  });
+
+  it('should return null with invalid userId', function() {
+    expect(service.generateId(null, 'b', 'c')).to.be.null;
+    expect(service.generateId(undefined, 'b', 'c')).to.be.null;
+  });
+
+  it('should return null with invalid serviceName', function() {
+    expect(service.generateId('a', null, 'c')).to.be.null;
+    expect(service.generateId('a', undefined, 'c')).to.be.null;
+  });
+
+  it('should return null with invalid serviceId', function() {
+    expect(service.generateId('a', 'b', null)).to.be.null;
+    expect(service.generateId('a', 'b', undefined)).to.be.null;
   });
 });
