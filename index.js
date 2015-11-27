8eccc266b82a09d66b504c487f0b90c7a21e4ee2
Qiming Fang <qf26@cornell.edu>
2015-11-27 02:07:15 -0800

    Revert "Implement next timestamp function." Does not compile on ironworker. https://app.asana.com/0/34520227311296/69287967936988
    This reverts commit f5db5bdc4b53c1dd1b5e800ede60a2bd023b9953.

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var time = require('time')
-
 module.exports = {
   generateId: function(userId, serviceName, serviceId) {
     if (userId == undefined || userId == null ||
@@ -25,25 +23,5 @@ module.exports = {
       serviceName: splitted[1],
       serviceId: splitted[2]
     };
-  },
-
-  /**
-   * currentTime is a {@code Date} object
-   * @return a {@code Date} object denoting when the next notification will come.
-   */
-  nextTimestamp: function(currentTime) {
-    var DEFAULT_TIME_ZONE = "America/Los_Angeles"
-
-    var nowInDefaultTimezone = new time.Date(currentTime.getTime());
-    nowInDefaultTimezone.setTimezone(DEFAULT_TIME_ZONE);
-    var hours = nowInDefaultTimezone.getHours()
-
-    if (hours < 15) {
-      nowInDefaultTimezone.setHours(18, 0, 0, 0);
-    } else {
-      nowInDefaultTimezone.setHours(18 + 24, 0, 0, 0);
-    }
-
-    return new Date(nowInDefaultTimezone.getTime())
   }
 }
